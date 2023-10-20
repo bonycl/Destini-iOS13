4ef0e52b38c684c15d1f0226f83273bafd588268
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-10-20 20:45:20 +0700

    setup

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+struct Story {
+    
+    
+    var story0 : String //= "You see a fork in the road."
+    var choice1 : String //= "Take a left."
+    var choice1Destination: Int
+    var choice2 : String //= "Take a right."
+    var choice2Destination: Int
+
+    init(title: String, choice1: String, choice1Destination: Int,choice2: String, choice2Destination: Int) {
+        self .story0 = title
+        self .choice1 = choice1
+        self .choice1Destination = choice1Destination
+        self .choice2 = choice2
+        self .choice2Destination = choice2Destination
+    }
+    
+}
+
